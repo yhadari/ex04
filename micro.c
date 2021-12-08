7614d87354c487b3b611f6aa8bdf10aae0593014
Yacine Hadari <yhadari@e1r2p3.1337.ma>
2021-12-08 22:55:56 +0100

    modify

--- micro.c
+++ micro.c
@@ -6,14 +6,20 @@
 /*   By: yhadari <yhadari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 13:26:21 by yhadari           #+#    #+#             */
-/*   Updated: 2021/12/07 20:49:05 by yhadari          ###   ########.fr       */
+/*   Updated: 2021/12/08 22:48:57 by yhadari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <sys/wait.h>
+
+typedef struct s_cmd{
+    
+    int pipe;
+    char *cmds[100];
+}               t_cmd;
 
 size_t ft_strlen(char *str){
     
@@ -49,27 +55,43 @@ int main(int argc, char **argv, char **envp){
     else{
         int i = 1;
         int j = 0;
+        int f = 0;
         int index = 0;
-        char *path = NULL;
-        char *cmd[100][100];
+        t_cmd *cmd = malloc(sizeof(t_cmd)*100);
+        memset(cmd, 0, sizeof(t_cmd)*100);
         while (argv[i]){
-            cmd[index][j++] = argv[i++];
-            if (argv[i] && strcmp(argv[i], ";") == 0){
+            cmd[index].cmds[j++] = argv[i++];
+            cmd[index].pipe = 0;
+            if (argv[i] && (strcmp(argv[i], ";") == 0 || strcmp(argv[i], "|") == 0)){
+                if (strcmp(argv[i], "|") == 0)
+                    cmd[index].pipe = 1;
                 i++;
                 index++;
                 j = 0;
             }
         }
         index = 0;
-        int f;
-        while (*cmd[index]){
-            path = *cmd[index];
+        int std[2];
+        int fd[2];
+        std[0] = dup(0);
+        std[1] = dup(1);
+        while (*cmd[index].cmds){
             f = fork();
             if (f == 0){
-                execve(path, cmd[index], envp);
-                exit(0);
+                if (cmd[index].pipe){
+                    pipe(fd);
+                    dup2(fd[0], 0);
+                    dup2(fd[1], 1);
+                }
+                else{
+                    dup2(std[0], 0);
+                    dup2(std[1], 1);
+                }
+                execve(*cmd[index].cmds, cmd[index].cmds, envp);
             }
             index++;
+            if (!cmd[index].pipe)
+                waitpid(f, NULL, 0);
         }
     }        
     return 0; 
